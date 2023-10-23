cf08138692a54039df1b599595e3ee8f66d42f91
Your Name aliciaxiong <aliciaxiong22@gmail.com>
2023-10-23 11:52:25 -0700

    homework

---
+++ notes.py
@@ -0,0 +1,6 @@
+class Animal:
+
+    def __init__(self, legs, height, weight): #init is the required methods when creating a class
+        self.legs = legs
+        self.height = height
+        super().init
